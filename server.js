b36de7ae02feeb8f2a523931a19a002b8001c273
Lilas-w <wangziweipku@pku.edu.cn>
2023-06-29 11:29:42 +0800

    initiate server.js to submodule

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
+const express = require('express')
+const { Sequelize, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize('clusters', 'test', '2023', {
   host: 'localhost',
   dialect: 'mysql',
-});
+})
 
 const Clusters = sequelize.define('clusters', {
   name: {
@@ -16,44 +16,45 @@ const Clusters = sequelize.define('clusters', {
     type: DataTypes.STRING,
     allowNull: true,
   },
-});
+})
 
-sequelize.sync()
+sequelize
+  .sync()
   .then(() => {
-    console.log('Database synchronized');
+    console.log('Database synchronized')
   })
   .catch(error => {
-    console.error('Error synchronizing database:', error);
-  });
+    console.error('Error synchronizing database:', error)
+  })
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.get('/clusters', (req, res) => {
   Clusters.findAll()
     .then(clusters => {
-      res.json(clusters);
+      res.json(clusters)
     })
     .catch(error => {
-      console.error('Error fetching clusters:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    });
-});
+      console.error('Error fetching clusters:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
+})
 
 app.post('/clusters', (req, res) => {
-  const { name, percentage } = req.body;
+  const { name, percentage } = req.body
   Clusters.create({ name, percentage })
     .then(cluster => {
-      res.json(cluster);
+      res.json(cluster)
     })
     .catch(error => {
-      console.error('Error creating cluster:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    });
-});
+      console.error('Error creating cluster:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
+})
 
 // Other routes...
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+  console.log('Server started on port 3000')
+})
