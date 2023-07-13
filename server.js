902a2e0afffaeb9f51f19bf69d3e1af2310399d3
Lilas-w <wangziweipku@pku.edu.cn>
2023-07-13 11:38:52 +0800

    add sessionID, post successfully but can't get

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const sequelize = new Sequelize('clusters', 'test', '2023', {
 
 // Create an instance of Sequelize and define the model
 const Clusters = sequelize.define('clusters', {
+  sessionID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: true,
@@ -22,7 +26,7 @@ const Clusters = sequelize.define('clusters', {
 
 // Sync the database and handle any errors
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(() => {
     console.log('Database synchronized')
   })
@@ -35,9 +39,18 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// Define the route to fetch all clusters
-app.get('/clusters', (req, res) => {
-  Clusters.findAll()
+// Define the route to generate a session ID
+app.get('/session', (req, res) => {
+  const sessionID = generateSessionID()
+
+  res.json({ sessionID })
+})
+
+// Define the route to fetch clusters for a specific session
+app.get('/clusters/:sessionID', (req, res) => {
+  const sessionID = req.params.sessionID
+
+  Clusters.findAll({ where: { sessionID } })
     .then(clusters => {
       res.json(clusters)
     })
@@ -50,35 +63,50 @@ app.get('/clusters', (req, res) => {
 // Define the route to create a new cluster
 app.post('/clusters', async (req, res) => {
   try {
-    await Clusters.destroy({ truncate: true })
-    console.log('All records in the clusters table deleted')
+    const { sessionID, data } = req.body;
 
-    const data = req.body
-
-    if (!Array.isArray(data)) {
-      res.status(400).json({ error: 'Invalid data format. Expected an array.' })
-      return
+    if (!sessionID || !Array.isArray(data)) {
+      res
+        .status(400)
+        .json({ error: 'Invalid request. Expected sessionID and data.' })
+      return;
     }
+
+    await Clusters.destroy({ where: { sessionID } });
+    console.log(
+      `All records in the clusters table deleted for session: ${sessionID}`,
+    )
 
     const createPromises = data.map(cluster => {
-      const { name, percentage } = cluster
-      return Clusters.create({ name, percentage })
-    })
+      return Clusters.create({
+        sessionID,
+        name: cluster.name,
+        percentage: cluster.percentage,
+      });
+    });
 
-    const createdClusters = await Promise.all(createPromises)
+    const createdClusters = await Promise.all(createPromises);
 
-    res.json(createdClusters)
+    res.json(createdClusters);
   } catch (error) {
     if (error.name === 'SequelizeUniqueConstraintError') {
-      res.status(400).json({ error: 'Cluster name must be unique' })
+      res.status(400).json({ error: 'Cluster name must be unique' });
     } else {
-      console.error('Error creating clusters:', error)
-      res.status(500).json({ error: 'Internal server error' })
+      console.error('Error creating clusters:', error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   }
-})
+});
 
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000')
 })
+
+function generateSessionID() {
+  const timestamp = Date.now().toString()
+  const random = Math.floor(Math.random() * 1000000)
+    .toString()
+    .padStart(6, '0')
+  return timestamp + random
+}
