5ab02c971afe8d66b86b9cfdb7c4e2a8be853cf0
Lilas-w <wangziweipku@pku.edu.cn>
2023-07-24 11:25:18 +0800

    format

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Server
 
-Server is a RESTful API for FOODIE-browser built using the Express framework that allows users to fetch and create clusters. It follows the principles of using standard HTTP methods (GET, POST) to perform actions on resources.
+Server is a RESTful API for FOODIE-browser built using the Express framework
+that allows users to fetch and create clusters. It follows the principles of
+using standard HTTP methods (GET, POST) to perform actions on resources.
 
 ## Getting Started
 
 To get started with the application, follow these steps:
 
-1. Clone the jbrowse-component repository: `git clone https://github.com/your-repo-url.git`
+1. Clone the jbrowse-component repository:
+   `git clone https://github.com/your-repo-url.git`
 2. Navigate to the server submodule: `cd jbrowse-component/server`
 3. Install the dependencies: `npm install`
-3. Configure the MySQL database connection in the `server.js` file.
-4. Start the server: `node server.js`
+4. Configure the MySQL database connection in the `server.js` file.
+5. Start the server: `node server.js`
 
 ## API Endpoints
 
@@ -21,35 +24,41 @@ This endpoint retrieves all clusters from the database.
 - **URL**: `/clusters`
 - **Method**: `GET`
 - **Request Body**: None
-- **Success Response**: 
-    - **Code**: 200
-    - **Content**: JSON array containing all clusters
+- **Success Response**:
+  - **Code**: 200
+  - **Content**: JSON array containing all clusters
 - **Error Response**:
-    - **Code**: 500
-    - **Content**: JSON object with the error message
+  - **Code**: 500
+  - **Content**: JSON object with the error message
 
 ### Create New Clusters
 
-This endpoint creates new clusters in the database. It first deletes all existing records in the "clusters" table, validates the request body data, and then inserts new records for each cluster.
+This endpoint creates new clusters in the database. It first deletes all
+existing records in the "clusters" table, validates the request body data, and
+then inserts new records for each cluster.
 
 - **URL**: `/clusters`
 - **Method**: `POST`
 - **Request Body**: JSON array containing the clusters data
-- **Success Response**: 
-    - **Code**: 200
-    - **Content**: JSON array containing the created clusters
+- **Success Response**:
+  - **Code**: 200
+  - **Content**: JSON array containing the created clusters
 - **Error Response**:
-    - **Code**: 400
-    - **Content**: JSON object with the error message (e.g., invalid data format)
-    - **Code**: 500
-    - **Content**: JSON object with the error message
+  - **Code**: 400
+  - **Content**: JSON object with the error message (e.g., invalid data format)
+  - **Code**: 500
+  - **Content**: JSON object with the error message
 
 ## Implementation Details
 
-The Sequelize package is used to interact with the MySQL database, define the model for the "clusters" table, and perform database operations. 
+The Sequelize package is used to interact with the MySQL database, define the
+model for the "clusters" table, and perform database operations.
 
-The Express framework is used to handle HTTP requests and define the API endpoints.
+The Express framework is used to handle HTTP requests and define the API
+endpoints.
 
-Please note that you need to configure the MySQL database connection and install the necessary dependencies before running the application.
+Please note that you need to configure the MySQL database connection and install
+the necessary dependencies before running the application.
 
-Feel free to explore and interact with the API endpoints to fetch and create clusters!
+Feel free to explore and interact with the API endpoints to fetch and create
+clusters!
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
-const cors = require('cors');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const { Sequelize, DataTypes } = require('sequelize')
+const cors = require('cors')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 const sequelize = new Sequelize('clusters', 'test', '2023', {
   host: 'localhost',
   dialect: 'mysql',
-});
+})
 
 // Create an instance of Sequelize and define the model
 const Clusters = sequelize.define('clusters', {
@@ -24,28 +24,28 @@ const Clusters = sequelize.define('clusters', {
     type: DataTypes.STRING,
     defaultValue: '',
   },
-});
+})
 
 // Sync the database and handle any errors
 sequelize
   .sync()
   .then(() => {
-    console.log('Database synchronized');
+    console.log('Database synchronized')
   })
-  .catch((error) => {
-    console.error('Error synchronizing database:', error);
-  });
+  .catch(error => {
+    console.error('Error synchronizing database:', error)
+  })
 
 // Create an Express app and enable JSON parsing
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 app.use(
   cors({
     origin: 'http://localhost:3001', // Set the allowed origin
     credentials: true, // Enable sending cookies and other credentials with the request
-  })
-);
-app.use(cookieParser());
+  }),
+)
+app.use(cookieParser())
 
 // Setup express-session middleware
 app.use(
@@ -54,56 +54,56 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false, httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }, // Set the cookie properties
-  })
-);
+  }),
+)
 
 // Define the route to fetch all clusters
 app.get('/clusters', (req, res) => {
-  const sessionId = req.session.id;
+  const sessionId = req.session.id
   Clusters.findAll({ where: { sessionId } })
-    .then((clusters) => {
-      res.json(clusters);
+    .then(clusters => {
+      res.json(clusters)
     })
-    .catch((error) => {
-      console.error('Error fetching clusters:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    });
-});
+    .catch(error => {
+      console.error('Error fetching clusters:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
+})
 
 app.post('/clusters', async (req, res) => {
   try {
-    const sessionId = req.session.id;
+    const sessionId = req.session.id
 
     // Delete all records from the clusters table
-    await Clusters.destroy({ truncate: true });
+    await Clusters.destroy({ truncate: true })
 
-    console.log('All records in the clusters table deleted');
+    console.log('All records in the clusters table deleted')
 
-    const data = req.body;
+    const data = req.body
 
     if (!Array.isArray(data)) {
-      res.status(400).json({ error: 'Invalid data format. Expected an array.' });
-      return;
+      res.status(400).json({ error: 'Invalid data format. Expected an array.' })
+      return
     }
 
-    const createPromises = data.map((cluster) => {
-      const { name, percentage } = cluster;
-      return Clusters.create({ name, percentage, sessionId });
-    });
+    const createPromises = data.map(cluster => {
+      const { name, percentage } = cluster
+      return Clusters.create({ name, percentage, sessionId })
+    })
 
-    const createdClusters = await Promise.all(createPromises);
-    res.json(createdClusters);
+    const createdClusters = await Promise.all(createPromises)
+    res.json(createdClusters)
   } catch (error) {
     if (error.name === 'SequelizeUniqueConstraintError') {
-      res.status(400).json({ error: 'Cluster name must be unique' });
+      res.status(400).json({ error: 'Cluster name must be unique' })
     } else {
-      console.error('Error creating clusters:', error);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error('Error creating clusters:', error)
+      res.status(500).json({ error: 'Internal server error' })
     }
   }
-});
+})
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+  console.log('Server started on port 3000')
+})
